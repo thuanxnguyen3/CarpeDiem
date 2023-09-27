d73d3d499e0b5120b53fef97b0f778d2d886ec4c
Thuan Nguyen <txn190017@utdallas.edu>
2023-09-27 18:25:57 -0500

    Finished the crafting system

--- Assets/Scripts/Crafting/CraftingManager.cs
+++ Assets/Scripts/Crafting/CraftingManager.cs
@@ -4,14 +4,43 @@ using UnityEngine;
 
 public class CraftingManager : MonoBehaviour
 {
+    private InventoryManager inventory;
+
+
     public RecipeTemplate recipeTemplate;
 
     public CraftingRecipeSO[] recipes;
     public Transform contentHolder;
 
+    public RecipeTemplate recipeInCraft;
+    private bool isCrafting;
+    private float currentTimer;
+
     private void Start()
     {
+        inventory = GetComponentInParent<InventoryManager>();
+
         GenerateRecipes();
+    }
+
+    private void Update()
+    {
+        if (isCrafting)
+        {
+            if (currentTimer > 0)
+            {
+                recipeInCraft.timerText.text = currentTimer.ToString("f2");
+            } else
+            {
+                recipeInCraft.timerText.text = "";
+
+                inventory.AddItem(recipeInCraft.recipe.outcome, recipeInCraft.recipe.outcomeAmount);
+
+                isCrafting = false;
+            }
+
+            currentTimer -= Time.deltaTime;
+        }
     }
 
     public void GenerateRecipes()
@@ -20,8 +49,10 @@ public class CraftingManager : MonoBehaviour
         {
             RecipeTemplate recipe = Instantiate(recipeTemplate.gameObject, contentHolder).GetComponent<RecipeTemplate>();
 
+            recipe.recipe = recipes[i];
             recipe.icon.sprite = recipes[i].icon;
             recipe.nameText.text = recipes[i].recipeName;
+            recipe.timerText.text = "";
 
             for (int b = 0; b < recipes[i].requirements.Length; b++)
             {
@@ -33,4 +64,124 @@ public class CraftingManager : MonoBehaviour
             }
         }
     }
+
+    public void Try_Craft(RecipeTemplate template)
+    {
+        if (!HasResources(template.recipe) || isCrafting)
+            return;
+
+        TakeResources(template.recipe);
+
+        recipeInCraft = template;
+        isCrafting = true;
+        currentTimer = template.recipe.craftingTime;
+    }
+
+    public void Cancel(RecipeTemplate template)
+    {
+        if (!isCrafting)
+            return;
+
+        for (int i = 0; i < template.recipe.requirements.Length; i++)
+        {
+            inventory.AddItem(template.recipe.requirements[i].data, template.recipe.requirements[i].amountNeeded);
+        }
+
+        isCrafting = false;
+
+        recipeInCraft.timerText.text = "";
+    }
+
+
+    public bool HasResources(CraftingRecipeSO recipe)
+    {
+        bool canCraft = true;
+
+        int[] stacksNeeded = null;
+        int[] stacksAvailable = null;
+        List<int> stacksNeededList = new List<int>();
+
+        //get stacks needed
+        for (int i = 0; i < recipe.requirements.Length; i++)
+        {
+            stacksNeededList.Add(recipe.requirements[i].amountNeeded);
+        }
+
+        stacksNeeded = stacksNeededList.ToArray();
+        stacksAvailable = new int[stacksNeeded.Length];
+
+        //check for items
+        for (int b = 0; b < recipe.requirements.Length; b++)
+        {
+            for (int i = 0; i < inventory.inventorySlots.Length; i++)
+            {
+                if (inventory.inventorySlots[i].data == recipe.requirements[b].data)
+                {
+                    stacksAvailable[b] += inventory.inventorySlots[i].stackSize;
+                }
+            }
+        }
+
+        //check if it can craft
+        for (int i = 0; i < stacksAvailable.Length; i++)
+        {
+            if (stacksAvailable[i] < stacksNeeded[i])
+            {
+                canCraft = false;
+                break;
+            }
+        }
+
+        return canCraft;
+
+    }
+
+    public void TakeResources(CraftingRecipeSO recipe)
+    {
+        int[] stacksNeeded = null;
+        List<int> stacksNeededList = new List<int>();
+
+        //get stacks needed
+        for (int i = 0; i < recipe.requirements.Length; i++)
+        {
+            stacksNeededList.Add(0);
+        }
+
+        stacksNeeded = stacksNeededList.ToArray();
+
+        //take items
+        for (int i = 0; i < recipe.requirements.Length; i++)
+        {
+            for (int b = 0; b < inventory.inventorySlots.Length; b++)
+            {
+                if (!inventory.inventorySlots[b].IsEmpty)
+                {
+                    if (inventory.inventorySlots[b].data == recipe.requirements[i].data)
+                    {
+                        if (stacksNeeded[i] < recipe.requirements[i].amountNeeded)
+                        {
+                            if (stacksNeeded[i] + inventory.inventorySlots[b].stackSize > recipe.requirements[i].amountNeeded)
+                            {
+                                int amountLeftOnSlot = (inventory.inventorySlots[b].stackSize + stacksNeeded[i]) - recipe.requirements[i].amountNeeded;
+
+                                inventory.inventorySlots[b].stackSize = amountLeftOnSlot;
+
+                                stacksNeeded[i] = recipe.requirements[i].amountNeeded;
+
+                            }
+                            else
+                            {
+                                stacksNeeded[i] += inventory.inventorySlots[b].stackSize;
+                                inventory.inventorySlots[b].Clean();
+                            }
+
+                        }
+
+                        inventory.inventorySlots[b].UpdateSlot();
+                    }
+                }
+            }
+        }
+    }
+
 }
--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -18,7 +18,7 @@ public class InventoryManager : MonoBehaviour
     public Transform contentHolder;
     public Transform hotbarContentHolder;
 
-    private Slot[] inventorySlots;
+    [HideInInspector] public Slot[] inventorySlots;
     private Slot[] hotbarSlots;
     //[SerializeField] private Slot[] allSlots;
 
@@ -283,6 +283,120 @@ public class InventoryManager : MonoBehaviour
         }
     }
 
+    public void AddItem(ItemSO data, int stackSize)
+    {
+        if (data.isStackable)
+        {
+            Slot stackableSlot = null;
+
+            // TRY FINDING STACKABLE SLOT
+            for (int i = 0; i < inventorySlots.Length; i++)
+            {
+                if (!inventorySlots[i].IsEmpty)
+                {
+                    if (inventorySlots[i].data == data && inventorySlots[i].stackSize < data.maxStack)
+                    {
+                        stackableSlot = inventorySlots[i];
+                        break;
+                    }
+
+                }
+            }
+
+            if (stackableSlot != null)
+            {
+
+                // IF IT CANNOT FIT THE PICKED UP AMOUNT
+                if (stackableSlot.stackSize + stackSize > data.maxStack)
+                {
+                    int amountLeft = (stackableSlot.stackSize + stackSize) - data.maxStack;
+
+
+
+                    // ADD IT TO THE STACKABLE SLOT
+                    stackableSlot.AddItemToSlot(data, data.maxStack);
+
+                    // TRY FIND A NEW EMPTY STACK
+                    for (int i = 0; i < inventorySlots.Length; i++)
+                    {
+                        if (inventorySlots[i].IsEmpty)
+                        {
+                            inventorySlots[i].AddItemToSlot(data, amountLeft);
+                            inventorySlots[i].UpdateSlot();
+
+                            break;
+                        }
+                    }
+
+                }
+                // IF IT CAN FIT THE PICKED UP AMOUNT
+                else
+                {
+                    stackableSlot.AddStackAmount(stackSize);
+
+                }
+
+                stackableSlot.UpdateSlot();
+            }
+            else
+            {
+                Slot emptySlot = null;
+
+
+                // FIND EMPTY SLOT
+                for (int i = 0; i < inventorySlots.Length; i++)
+                {
+                    if (inventorySlots[i].IsEmpty)
+                    {
+                        emptySlot = inventorySlots[i];
+                        break;
+                    }
+                }
+
+                // IF WE HAVE AN EMPTY SLOT THEN ADD THE ITEM
+                if (emptySlot != null)
+                {
+                    emptySlot.AddItemToSlot(data, stackSize);
+                    emptySlot.UpdateSlot();
+
+                }
+                else
+                {
+                    DropItem(data, stackSize);
+                }
+            }
+
+        }
+        else
+        {
+            Slot emptySlot = null;
+
+
+            // FIND EMPTY SLOT
+            for (int i = 0; i < inventorySlots.Length; i++)
+            {
+                if (inventorySlots[i].IsEmpty)
+                {
+                    emptySlot = inventorySlots[i];
+                    break;
+                }
+            }
+
+            // IF WE HAVE AN EMPTY SLOT THEN ADD THE ITEM
+            if (emptySlot != null)
+            {
+                emptySlot.AddItemToSlot(data, stackSize);
+                emptySlot.UpdateSlot();
+
+            }
+            else
+            {
+                DropItem(data, stackSize);
+            }
+
+        }
+    }
+
     public void DropItem(Slot slot)
     {
         Pickup pickup = Instantiate(dropModel, dropPos).AddComponent<Pickup>();
@@ -296,6 +410,16 @@ public class InventoryManager : MonoBehaviour
         slot.Clean();
     }
 
+    public void DropItem(ItemSO data, int stackSize)
+    {
+        Pickup pickup = Instantiate(dropModel, dropPos).AddComponent<Pickup>();
+        pickup.transform.position = dropPos.position;
+        pickup.transform.SetParent(null);
 
+
+        pickup.data = data;
+        pickup.stackSize = stackSize;
+
+    }
 
 }
