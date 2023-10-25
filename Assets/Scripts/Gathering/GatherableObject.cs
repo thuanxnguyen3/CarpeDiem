98f02b257a7b9c376e81721640eec725238150e1
Thuan Nguyen <txn190017@utdallas.edu>
2023-10-24 22:11:05 -0500

    Finished Tree Falling Down
    Implemented Tree Death Animation after the Player destroy the tree

--- Assets/Scripts/Gathering/GatherableObject.cs
+++ Assets/Scripts/Gathering/GatherableObject.cs
@@ -4,19 +4,45 @@ using UnityEngine;
 
 public class GatherableObject : MonoBehaviour
 {
+    public enum DeathType { Destroy, EnablePhysics}
+    public DeathType deathType;
+
     public GatherDataSO[] gatherDatas;
     public int hits;
     public ItemSO[] preferredTools;
     public int toolMultiplier = 2;
 
+    bool hasDied;
+
     private void Update()
     {
-        if (hits <= 0)
-            Destroy(gameObject);
+        if (hits <= 0 && !hasDied)
+        {
+            if (deathType == DeathType.Destroy)
+                Destroy(gameObject);
+            else if(deathType == DeathType.EnablePhysics)
+            {
+                if (GetComponent<Rigidbody>() != null)
+                {
+                    GetComponent<Rigidbody>().isKinematic = false;
+                    GetComponent<Rigidbody>().useGravity = true;
+
+                    GetComponent<Rigidbody>().AddTorque(Vector3.right * 50);
+                    Destroy(gameObject, 10f);
+                }
+                else
+                    Destroy(gameObject);
+            }
+
+            hasDied = true;
+        }
     }
 
     public void Gather (ItemSO toolUsed, InventoryManager inventory)
     {
+        if (hits <= 0)
+            return;
+
         bool usingRightTool = false;
 
         // check for tool
