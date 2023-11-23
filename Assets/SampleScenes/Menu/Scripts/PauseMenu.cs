f17065a5fdbb39cf4ccec4716ef18b6538981d20
Thuan Nguyen <txn190017@utdallas.edu>
2023-11-23 13:14:06 -0600

    Finished Background Music and Settings Menu

--- Assets/SampleScenes/Menu/Scripts/PauseMenu.cs
+++ Assets/SampleScenes/Menu/Scripts/PauseMenu.cs
@@ -4,15 +4,28 @@ using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
-
+    public GameObject pauseMenu;
+    public GameObject settingsMenu;
 
     public void Resume()
     {
         Time.timeScale = 1f;
         Player player = GetComponentInParent<Player>();
         player.GetComponent<PlayerStats>().isPaused = false;
-        gameObject.SetActive(false);
+        pauseMenu.gameObject.SetActive(false);
         
+    }
+
+    public void SettingsMenu()
+    {
+        settingsMenu.gameObject.SetActive(true);
+        pauseMenu.gameObject.SetActive(false);
+    }
+
+    public void BackToMenu()
+    {
+        settingsMenu.gameObject.SetActive(false);
+        pauseMenu.gameObject.SetActive(true);
     }
 
 
--- Assets/Scripts/UI/GameMusicManager.cs
+++ Assets/Scripts/UI/GameMusicManager.cs
@@ -5,32 +5,25 @@ using UnityEngine.Audio;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class MusicManager : MonoBehaviour
+public class GameMusicManager : MonoBehaviour
 {
     public AudioMixer audioMixer;
     public Slider slider;
-    public float mainMenuVolume;
-    public static MusicManager instance;
-    void Awake()
-    {
-        if (instance != null)
-            Destroy(gameObject);
-        else
-        {
-            instance = this;
-            DontDestroyOnLoad(this.gameObject);
-        }
-    }
+    private MusicManager musicManager;
 
+    private void Awake()
+    {
+        musicManager = GetComponent<MusicManager>();
+    }
     private void Start()
     {
-        audioMixer.SetFloat("Volume", 0);
+        slider.value = musicManager.mainMenuVolume;
+        audioMixer.SetFloat("Volume", musicManager.mainMenuVolume);
     }
 
     public void SetVolume(float volume)
     {
         volume = slider.value;
-        mainMenuVolume = slider.value;
         audioMixer.SetFloat("Volume", volume);
     }
 }
